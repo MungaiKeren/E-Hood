7232258265bc50b10745f38ff814ac4a0a6f81c2
MungaiKeren <wambukeren@gmail.com>
2019-10-25 15:52:58 +0300

    This commit creates templates but not yet registered

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    title = "Neighbourhood"
+    context = {
+        "title": title,
+    }
+    return render(request, 'index.html', context)
