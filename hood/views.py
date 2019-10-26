7516c35832261f9edfd21d132212777b76d76d71
MungaiKeren <wambukeren@gmail.com>
2019-10-26 14:45:57 +0300

    This commit creates a register view

--- hood/views.py
+++ hood/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -10,3 +11,9 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+def register(request):
+    form = UserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'register/signUp.html', context)
