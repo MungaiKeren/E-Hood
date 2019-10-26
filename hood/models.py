c60e0183a77fa1df40131f78ccd1d6a6f4c243b9
MungaiKeren <wambukeren@gmail.com>
2019-10-26 17:29:22 +0300

    this commit updates various images and changes the dunda functions in models

--- hood/models.py
+++ hood/models.py
@@ -8,25 +8,31 @@ class Hood(models.Model):
     location = models.CharField(max_length=20)
     image = models.ImageField(upload_to='hood_photo', blank=True, default='hood_photo/hood.jpg')
     occupants = models.PositiveSmallIntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile_photo', blank=True, default='profile_photo/defaultprofile.jpg')
-    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, default='myHood')
     bio = models.CharField(max_length=255, blank=True)
     contacts = models.CharField(max_length=200)
+    join_date = models.DateTimeField(auto_now_add=True)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, default='myHood')
 
     def __str__(self):
-        return self.hood
+        return f'{self.user.username}'
 
 
 class Business(models.Model):
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=20)
+    create_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='biz_pic/', blank=True, default='biz_pic/bizpic.jpg')
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, default='no business')
     hood = models.ForeignKey(Hood, on_delete=models.CASCADE, default='myHood')
+
+    def __str__(self):
+        return f'{self.name}'
