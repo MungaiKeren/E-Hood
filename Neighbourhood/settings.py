cb8a4fa042e0b35b267b05cd1c87b0930fd0be6c
MungaiKeren <wambukeren@gmail.com>
2019-10-26 17:04:10 +0300

    This commit creates hood, profile and business models

--- Neighbourhood/settings.py
+++ Neighbourhood/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Hood(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='hood_photo', blank=True, default='hood_photo/hood.jpg')
+    occupants = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_photo', blank=True, default='profile_photo/defaultprofile.jpg')
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, default='myHood')
+    bio = models.CharField(max_length=255, blank=True)
+    contacts = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.hood
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='biz_pic/', blank=True, default='biz_pic/bizpic.jpg')
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, default='no business')
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, default='myHood')
