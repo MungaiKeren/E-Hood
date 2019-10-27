9c252f9ae259f2e666425cd77a500605efa9c029
MungaiKeren <wambukeren@gmail.com>
2019-10-27 14:41:41 +0300

    This commit creates a animation effect in the create profile page
    reshufle contacts position

--- hood/forms.py
+++ hood/forms.py
@@ -24,4 +24,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["image", "bio", "contacts", "hood"]
+        fields = ["contacts", "image", "bio", "hood"]
